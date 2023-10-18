8b030e3cc7838953a0bb8c1c35067cb0b4d47fdc
Shazzad Hossain Mazumder <shmazumder23@gmail.com>
2023-10-18 10:08:13 +0600

    Update sayhi.php

--- sayhi.php
+++ sayhi.php
@@ -1,4 +1,13 @@
 <?php
+// first-question: yes
+// name: Shazzad Hossain Mazumder
+// multi[]: Rock
+// multi[]: Pop
+// multi[]: Country
+// multi[]: Classic
+// programmer: yes
+// email: shmazumder23@gmail.com
+// password: 1234
 
 // if "email" variable is filled out, send email
 if (isset($_POST['email'])) {
@@ -13,7 +22,7 @@ if (isset($_POST['email'])) {
     mail($to, $subject, $message, "From:" . $email);
 
     //Email response
-    echo "Thank you! A email will be sent to you soon!";
+    echo "Thank you! An email will be sent to you soon!";
 } else {
     echo "Oops! Something went south...";
 }
